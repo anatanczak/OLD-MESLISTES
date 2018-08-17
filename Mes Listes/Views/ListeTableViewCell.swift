c619bca41e9c4716a4e72328a508d466523d4b68
Ana Viktoriv <anaviktoriv@gmail.com>
2018-08-17 14:51:12 +0200

    add ListeTableViewCell

---
+++ Mes Listes/Views/ListeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListeTableViewCell.swift
+//  Mes Listes
+//
+//  Created by Anastasiia Tanczak on 17/08/2018.
+//  Copyright © 2018 Ana Viktoriv. All rights reserved.
+//
+
+import UIKit
+
+class ListeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
